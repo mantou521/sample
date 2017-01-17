b18aee00927e16e47acf932a108d943d4993c411
aiker <519893064@qq.com>
2017-01-17 09:28:09 +0000

    Add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
@@ -57,9 +58,9 @@ class UsersController extends Controller
             'password' => $request->password,
         ]);
 
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮箱已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
     }
 
     public function edit($id)
@@ -98,4 +99,30 @@ class UsersController extends Controller
         return back();
     }
 
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = '519893064@qq.com';
+        $name = 'Aiker';
+        $to = $user->email;
+        $subject = "感谢注册 Sample 应用！请确认您的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -19,7 +19,8 @@
                                         <form action="{{ route('logout') }}" method="POST">
                                             {{ csrf_field() }}
                                             {{ method_field('DELETE') }}
-                                            <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                            <button class="btn btn-block btn-danger" type="submit" name="button">退出
+                                            </button>
                                         </form>
                                     </a>
                                 </li>
